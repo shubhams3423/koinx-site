743d111290f8599fe0c96a70decf9a07e28dcd39
shubhams3423 <shubham1844s@gmail.com>
2024-03-05 16:50:48 +0530

    chore: react setup and tailwind

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-center pt-5">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
